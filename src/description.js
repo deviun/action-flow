224e1fed149d7bad836e9387e0d5a0499da573ce
Anton Danilov <a.deviun@gmail.com>
2018-07-21 15:42:51 +0300

    move description class to file

---
+++ src/description.js
@@ -0,0 +1,27 @@
+const $sha256 = require('sha256');
+
+/**
+ * @description Create hash id for description object of operation
+ * @returns {String} sha256 hash string
+ */
+class Description {
+  static parse(flowDescription) {
+    if (!(flowDescription instanceof Object)) {
+      throw new Error(`[${moduleName}] {flowDescription} -> object is not found`);
+    }
+
+    function _p(obj) {
+      return Object.keys(obj).reduce((r, key, index) => {
+        const value = obj[key] instanceof Object ? _p(obj[key]) : String(obj[key]);
+
+        r += $sha256(`${key}.${value}.${index}`);
+
+        return r;
+      }, 'start');
+    }
+
+    return $sha256(_p(flowDescription));
+  }
+}
+
+module.exports = Description;
