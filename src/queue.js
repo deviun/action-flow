ddbdf985dda67cd3269f2c4fb038ede6fa4fde5f
Anton Danilov <a.deviun@gmail.com>
2018-07-21 15:43:58 +0300

    refactory queue class

--- src/queue.js
+++ src/queue.js
@@ -1,48 +1,22 @@
-const {afModelsGetProp} = require('./models');
+const get = require('lodash/get');
+const has = require('lodash/has');
 
-const DB_QUEUE = afModelsGetProp('queue');
-
-const Time = {
-  getSec () {
-    return (new Date()).getTime() / 1000;
-  },
-  getMsec () {
-    return (new Date()).getTime();
-  }
-};
+const DEFAULT_DRIVER = 'mongodb';
 
 class Queue {
   constructor (options) {
     Object.assign(this, options);
 
-    this.queue = this.db.collection(DB_QUEUE);
-  }
+    this.driverName = get(options.driver, 'driverName', DEFAULT_DRIVER);
 
-  async join () {
-    await this.leave();
+    if (has(options, 'driverClass')) {
+      Object.assign(this, options.driverClass);
+    } else {
+      // try connect exists driver
+      const driverClass = require('./drivers/' + this.driverName);
 
-    const me = {
-      clientId: this.clientId,
-      descriptionHash: this.descriptionHash,
-      setTime: Time.getMsec()
-    };
-
-    return await this.queue.insert(me);
-  }
-
-  async isFirst () {
-    const firstClient = await this.queue.findOne({
-      descriptionHash: this.descriptionHash
-    });
-
-    return ( firstClient.clientId === this.clientId );
-  }
-
-  async leave () {
-    return await this.queue.deleteMany({
-      clientId: this.clientId,
-      descriptionHash: this.descriptionHash
-    });
+      Object.assign(this, driverClass);
+    }
   }
 }
 
