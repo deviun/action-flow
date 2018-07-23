a972bc233215d9a20fc42d60244c171df5cef608
Anton Danilov <a.deviun@gmail.com>
2018-07-23 10:32:14 +0300

    add logging

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,6 @@
 const get = require('lodash/get');
 const has = require('lodash/has');
+const $log = require('./libs/log');
 
 const DEFAULT_DRIVER = 'mongodb';
 
@@ -7,7 +8,9 @@ class Queue {
   constructor (options) {
     Object.assign(this, options);
 
-    this.driverName = get(options.driver, 'driverName', DEFAULT_DRIVER);
+    $log.debug('[action-flow] options: %s', JSON.stringify(options));
+
+    this.driverName = get(options, 'driverName', DEFAULT_DRIVER);
 
     if (has(options, 'driverClass')) {
       this.driverClass = options.driverClass;
@@ -17,6 +20,8 @@ class Queue {
     }
 
     this.driver = new this.driverClass(options);
+
+    $log.debug('[action-flow] use driver "%s"', options.driverName);
 
     // connect required methods from driver
     this.join = this.driver.join.bind(this.driver);
